ec99d6c5d144ac4d81393d393d2e531e5b1597f1
vinh Ng <vinhng.pc19@gmail.com>
2024-02-21 16:01:43 +0700

    dio

--- lib/remote_config/api_manager/rest_client.dart
+++ lib/remote_config/api_manager/rest_client.dart
@@ -1,16 +1,17 @@
 import 'dart:convert';
 
+import 'package:dio/dio.dart';
 import 'package:grocery/models/product_model.dart';
 import 'package:http/http.dart' as http;
 
-String baseUrl = 'http://192.168.1.7:3000/';
+String baseUrl = 'http://192.168.1.10:3000/';
 
 class RestClient {
+  final dio = Dio();
   Future<ProductModels> getListProducts({required int pageNumber}) async {
-    final response =
-        await http.get(Uri.parse('${baseUrl}product/?page=$pageNumber'));
+    final response = await dio.get('${baseUrl}product/?page=$pageNumber');
     if (response.statusCode == 200) {
-      final listProducts = ProductModels.fromJson(jsonDecode(response.body));
+      final listProducts = ProductModels.fromJson(response.data);
       return listProducts;
     } else {
       throw Exception('Failed to load album');
